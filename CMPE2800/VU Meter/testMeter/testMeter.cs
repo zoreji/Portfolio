2213a9158796c2545581d6d1bbe59969339fb90a
Ervin Hernandez <zoreji@users.noreply.github.com>
2016-05-10 11:38:13 -0600

    Add files via upload

---
+++ CMPE2800/VU Meter/testMeter/testMeter.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+namespace testMeter
+{
+    public partial class testMeter: UserControl
+    {
+        float _minLvl = -60;
+        [Browsable(true), Description("Minimum Level"), Category("Meter Level")]
+        public float minLvl
+        {
+            get { return _minLvl; }
+            set { _minLvl = value < _maxLvl ? value : _maxLvl; Invalidate(); }
+        }
+        float _maxLvl = 1;
+        [Browsable(true), Description("Maximum Level"), Category("Meter Level")]
+        public float maxLvl
+        {
+            get { return _maxLvl; }
+            set { _maxLvl = value > _minLvl ? value : _minLvl; Invalidate(); }
+        }
+        PointF _value = new PointF();
+        [Browsable(true), Description("Value..."), Category("Meter Level")]
+        public PointF Value
+        {
+            get { return _value; }
+            set { _value = value; Invalidate(); }
+        }
+        float _alert_Lvl = 0;
+        [Browsable(true), Description("(╯°□°）╯︵ ┻━┻"), Category("Meter Level")]
+        public float alert_Lvl
+        {
+            get { return _alert_Lvl; }
+            set { _alert_Lvl = value; Invalidate(); }
+        }
+        int Meter_type = 0;
+        [Browsable(true), Description("The Meter Type"), Category("Meter Type")]
+
+        float right_degrees = 0;
+        [Browsable(true), Description("Degrees of the line arc of right"), Category("Meter")]
+        public float rg_degrees
+        {
+            get { return right_degrees; }
+            set { right_degrees = value + 30; Invalidate(); }
+        }
+        float left_degrees = 0;
+        [Browsable(true), Description("Degrees of the line arc of left"), Category("Meter")]
+        public float lf_degrees
+        {
+            get { return left_degrees; }
+            set { left_degrees = value + 30; Invalidate(); }
+        }
+        float _dB = 0;
+        [Browsable(true), Description("Values of the dB in float"), Category("Meter")]
+        public float dB
+        {
+            get { return _dB; }
+            set { _dB = value; Invalidate(); }
+        }
+        public testMeter()
+        {
+            InitializeComponent();
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //base.OnPaintBackground(e);
+
+        }
+
+        private void transferFunction()
+        {
+            lf_degrees = Value.Y;
+            right_degrees = Value.X;
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            
+            using (BufferedGraphicsContext bgc = new BufferedGraphicsContext())
+            {
+                using (BufferedGraphics bg = bgc.Allocate(CreateGraphics(), ClientRectangle))
+                {
+                    gr = bg.Graphics;
+                    Meter(gr);
+                    gr.DrawPath(Pens.Yellow, moveright());
+                    gr.FillPath(new SolidBrush(Color.Yellow), moveright());
+                    gr.DrawPath(Pens.Yellow, moveleft());
+                    gr.FillPath(new SolidBrush(Color.Yellow), moveleft());
+                    bg.Render();
+                }
+            }
+        }
+        private GraphicsPath moveright()
+        {
+            GraphicsPath gp = (GraphicsPath)line().Clone();
+            Matrix mat = new Matrix();
+            mat.Translate(170, 200);
+            mat.RotateAt((float)right_degrees, new PointF(0, 0));
+            gp.Transform(mat);
+            //Pen p = new Pen(Color.Yellow);
+            return gp;
+            
+        }
+        private GraphicsPath moveleft()
+        {
+            GraphicsPath gp = (GraphicsPath)line().Clone();
+            Matrix mat = new Matrix();
+            mat.Translate(570, 200);
+            mat.RotateAt((float)left_degrees, new PointF(0, 0));
+            gp.Transform(mat);
+            //Pen p = new Pen(Color.Yellow);
+            return gp;
+        }
+        private void Meter(Graphics gr)
+        {
+            if (gr == null)
+                gr = CreateGraphics();
+            gr.Clear(Color.Black);
+            gr.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            if (right_degrees > 120)
+                gr.DrawArc(new Pen(Color.Red, 3), 20, 50, 300, 300, 210, 110);
+            else
+                gr.DrawArc(new Pen(Color.DarkRed, 3), 20, 50, 300, 300, 210, 110);
+            if (left_degrees > 120) 
+                gr.DrawArc(new Pen(Color.Red, 3), 420, 50, 300, 300, 210, 110);
+            else
+                gr.DrawArc(new Pen(Color.DarkRed, 3), 420, 50, 300, 300, 210, 110);
+            gr.DrawArc(new Pen(Color.Blue, 3), 20, 50, 300, 300, 210, 90);
+            gr.DrawArc(new Pen(Color.Blue, 3), 420, 50, 300, 300, 210, 90);
+        }
+        private GraphicsPath line()
+        {
+            GraphicsPath gc = new GraphicsPath();
+            PointF p = new PointF(-100, 0);
+            PointF testp = new PointF(-175, 0);
+            gc.AddLine(p, testp);
+            return gc;
+        }
+        private void math()
+        {
+
+        }
+    }
+}
---
+++ CMPE2800/VU Meter/testStatus/testStatus.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace testStatus
+{
+    public partial class testStatus: UserControl
+    {
+        Thread enQ;
+        Thread calAvg;
+        object key = new object(); // master key
+        Queue<PointF> Avg;
+        List<float> left;
+        List<float> right;
+        int Limit = 8;
+        float _minLvl = 0;
+        [Browsable(true), Description("Minimum Level"), Category("Meter Level")]
+        public float minLvl
+        {
+            get { return _minLvl; }
+            set { _minLvl = value < _maxLvl ? value : _maxLvl; Invalidate(); }
+        }
+        float _maxLvl = 1;
+        [Browsable(true), Description("Maximum Level"), Category("Meter Level")]
+        public float maxLvl
+        {
+            get { return _maxLvl; }
+            set { _maxLvl = value > _minLvl ? value : _minLvl; Invalidate(); }
+        }
+        PointF _value = new PointF();
+        [Browsable(true), Description("Value..."), Category("Meter Level")]
+        public PointF Value
+        {
+            get { return _value; }
+            set { _value = value; enque(_value); Invalidate(); }
+        }
+        float _alert_Lvl = 0;
+        [Browsable(true), Description("(╯°□°）╯︵ ┻━┻"), Category("Meter Level")]
+        public float alert_Lvl
+        {
+            get { return _alert_Lvl; }
+            set { _alert_Lvl = value; Invalidate(); }
+        }
+        public enum Meter_type { Peak = 1,Fast,Medium,Slow}
+        Meter_type _speed = Meter_type.Medium;
+        [Browsable(true), Description("The Meter Type"), Category("Meter Type")]
+        public Meter_type Speed
+        {
+            get { return _speed; }
+            set { _speed = value; Invalidate(); }
+        }
+
+        float right_degrees = 1;
+        [Browsable(true), Description("Degrees of the line arc of right"), Category("Meter")]
+        public float rg_degrees
+        {
+            get { return right_degrees; }
+            set { right_degrees = value; Invalidate(); }
+        }
+        float left_degrees = 1;
+        [Browsable(true), Description("Degrees of the line arc of left"), Category("Meter")]
+        public float lf_degrees
+        {
+            get { return left_degrees; }
+            set { left_degrees = value; Invalidate(); }
+        }
+        float _dB = 0;
+        [Browsable(true), Description("Values of the dB in float"), Category("Meter")]
+        public float dB
+        {
+            get { return _dB; }
+            set { _dB = value; Invalidate(); }
+        }
+        float left_avgLvl = 0;
+        [Browsable(true), Description("The average left values of the raw data"), Category("Meter")]
+        public float LeftavgLvl
+        {
+            get { return left_avgLvl; }
+            set { left_avgLvl = value; Invalidate(); }
+        }
+        float right_avgLvl = 0;
+        [Browsable(true), Description("The average right values of the raw data"), Category("Meter")]
+        public float RightavgLvl
+        {
+            get { return right_avgLvl; }
+            set { right_avgLvl = value; Invalidate(); }
+        }
+        Font f = null;
+        public testStatus()
+        {
+            InitializeComponent();
+            f = new Font("Comic Sans", 14);
+            //Avg = new Queue<PointF>();
+            left = new List<float>();
+            right = new List<float>();
+
+        }
+        public event EventHandler onAlarm = null;
+
+        public delegate void delVoidInt(int value);
+        public event delVoidInt alarmCount = null;
+        public event EventHandler changeSpeed = null;
+        public delegate void delVoidtype(int value);
+        public event EventHandler changetype = null;
+        private void checkalert(PointF value)
+        {
+            if (value.X > alert_Lvl || value.Y > alert_Lvl)
+                onAlarm?.Invoke(this, new EventArgs());
+
+        }
+        protected override void OnPaintBackground(PaintEventArgs e)
+        {
+            //base.OnPaintBackground(e);
+        }
+        private void transferFunction()
+        {
+            double leftd = (20 * Math.Log10((double)left_avgLvl));
+            double rightd = (20 * Math.Log10((double)right_avgLvl));
+            checkalert(_value);
+            left_degrees = (float)leftd;
+            right_degrees = (float)rightd;
+        }
+        private void setLimit(Meter_type type)
+        {
+            switch (type)
+            {
+                case Meter_type.Peak:
+                    Limit = 1;
+                    break;
+                case Meter_type.Fast:
+                    Limit = 3;
+                    break;
+                case Meter_type.Medium:
+                    Limit = 8;
+                    break;
+                case Meter_type.Slow:
+                    Limit = 12;
+                    break;
+            }
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            Graphics gr = e.Graphics;
+            setLimit(_speed);
+            sampleAverage();
+            data(gr);
+        }
+        private void enque(PointF value)
+        {
+            //Avg.Enqueue(value);
+            left.Add(value.X);
+            right.Add(value.Y);
+        }
+        private void sampleAverage()
+        {
+            float leftsum = 0;
+            float rightsum = 0;
+            if (left.Count < Limit || right.Count < Limit)
+                return;
+            foreach (var x in left)
+                leftsum += x;
+            foreach (var y in right)
+                rightsum += y;
+            left_avgLvl = leftsum / Limit;
+            right_avgLvl = rightsum / Limit;
+            transferFunction();
+            left.Clear();
+            right.Clear();
+        }
+        private void data(Graphics gr)
+        {
+            //sampleAverage();
+            if (gr == null)
+                gr = CreateGraphics();
+            gr.Clear(Color.Black);
+            gr.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAlias;
+            PointF pf = new PointF(10, 10);
+            gr.DrawString("Max:     " + _maxLvl.ToString(), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 30);
+            gr.DrawString("Min:     " + _minLvl.ToString(), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 50);
+            gr.DrawString("Alert:   " + _alert_Lvl.ToString(), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 70);
+            gr.DrawString("Value-right:   " + _value.Y.ToString(), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 90);
+            gr.DrawString("Value-left:   " + _value.X.ToString(), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 110);
+            gr.DrawString("Left:   " + left_degrees.ToString("F1"), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 130);
+            gr.DrawString("Right:   " + right_degrees.ToString("F1"), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 150);
+            gr.DrawString("Speed:   " + _speed.ToString(), f, new SolidBrush(Color.White), pf);
+            pf = new PointF(10, 170);
+        }
+    }
+}
